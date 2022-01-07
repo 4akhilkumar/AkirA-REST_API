1be6671244b0235d7915b1aa27923ba5ab5021a2
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2022-01-07 18:07:59 +0530

    getEncryptionData, getDecryptionData

--- akira_security_api/urls.py
+++ akira_security_api/urls.py
@@ -3,4 +3,6 @@ from akira_security_api import views
 
 urlpatterns = [
     path('getMetaData/<str:MetaKey>/<str:EncryptedMetaKey>/', views.IndexView.as_view(), name='index'),
+    path('getEncryptionData/<str:MetaKey>/', views.CustomEncryption.as_view()),
+    path('getDecryptionData/<str:EncryptedMetaData>/', views.CustomDecryption.as_view()),
 ]
--- akira_security_api/views.py
+++ akira_security_api/views.py
@@ -5,6 +5,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 import re
+import secrets
+import random
 
 class IndexView(APIView):
     def get(self, request, MetaKey, EncryptedMetaKey):
@@ -38,5 +40,54 @@ class IndexView(APIView):
         data = {
             'checkKeyEncrypted': checkKeyEncrypted,
             'MetaKey': password,
+        }
+        return Response(data)
+
+class CustomEncryption(APIView):
+    def get(self, request, MetaKey):
+        RanSalt = secrets.token_urlsafe(90)
+        SpecialChracter = ["@", "#", "(", ")"] * len(MetaKey)
+        EncryptedUsername = ""
+        for i in range(len(MetaKey)):
+            EncryptedUsername += chr(ord(MetaKey[i]) + 468)
+            
+        for i in range(len(SpecialChracter)):
+            ran = random.randint(0, len(RanSalt) - 1)
+            RanSalt = RanSalt[:ran] + SpecialChracter[i] + RanSalt[ran:]
+
+        SpecialChracterPos = []
+        for i in range(len(RanSalt)):
+            if RanSalt[i] in SpecialChracter:
+                SpecialChracterPos.append(i+1)
+        
+        randomPositions = random.sample(SpecialChracterPos, (len(MetaKey)))
+        randomPositions.sort()
+
+        to_modify = list(RanSalt)
+        replacements = list(EncryptedUsername)
+
+        for (index, replacement) in zip(randomPositions, replacements):
+            to_modify[index] = replacement
+        
+        FinalEncryptedUsername = "".join(to_modify)
+        FinalEncryptedUsername = re.sub('[@#()]+', '', FinalEncryptedUsername)
+
+        data = {
+            'EncryptedUsername': FinalEncryptedUsername,
+        }
+        return Response(data)
+
+class CustomDecryption(APIView):
+    def get(self, request, EncryptedMetaData):
+        ep = EncryptedMetaData
+        afterRAN = re.sub('[0-9a-zA-Z]+', '', ep)
+        afterRAN = re.sub('[-_]+', '', afterRAN)
+
+        DecryptedUsername = ""
+        for i in range(len(afterRAN)):
+            DecryptedUsername += chr(ord(afterRAN[i]) - 468)
+
+        data = {
+            'DecryptedUsername': DecryptedUsername,
         }
         return Response(data)
