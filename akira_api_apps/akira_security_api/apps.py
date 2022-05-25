fff27e911b41dcfe56723bd8cbdbec41f848062d
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2022-05-25 20:40:01 +0530

    Moved the akira_security_api into the folder

--- akira_api_apps/akira_security_api/apps.py
+++ akira_api_apps/akira_security_api/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AkiraSecurityApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'akira_security_api'
+    name = 'akira_api_apps.akira_security_api'
--- akira_api_apps/akira_security_api/urls.py
+++ akira_api_apps/akira_security_api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from akira_security_api import views
+from . import views
 
 urlpatterns = [
     path('fetchKey/', views.fetchKey, name='fetchKey'),
